83c14e3077c4ba61b47f99015f27f4c3fc8ca7ff
Allison G. Endres <aendres19@gmail.com>
2017-03-29 16:34:42 -0400

    Add program to square and cube inputed number.

--- Squaring_Cubing/Program.cs
+++ Squaring_Cubing/Program.cs
@@ -8,8 +8,61 @@ namespace Squaring_Cubing
 {
     class Program
     {
+        public static int NumberSquared(int num)
+        {
+            int square = num * num;
+            Console.WriteLine("Square: " + square);
+            return square;
+        }
+       
+        public static int NumberCubed(int num)
+        {
+            int cube = num * num * num;
+            Console.WriteLine("Cube: " + cube);
+            return cube;
+        }
+
+        public static bool Continue()
+        {
+            Console.WriteLine("Continue? (y/n)");
+            string input = Console.ReadLine();
+            input.ToLower();
+            bool run;
+
+            if (input == "n")
+            {
+                Console.WriteLine("Goodbye!");
+                run = false;
+            }
+            else if (input == "y")
+            {
+                run = true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that is not a vaild input.");
+                run = Continue();
+            }
+            return run;
+        }
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please enter the number you would like to square and cube: ");
+            int num = int.Parse(Console.ReadLine());
+
+            bool run = true;
+
+
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine($"The cube and square of {i} is: ");
+                NumberSquared(i);
+                NumberCubed(i);
+               
+            }
+            run = Continue();
+
         }
     }
 }
